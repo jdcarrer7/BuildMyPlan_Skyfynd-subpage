028f8ef94173eee3a935bb0c7eb5d15219663142
jdcarrer7 <jdcarrero7@gmail.com>
2026-01-12 14:56:43 -0800

    Fix header links to point to production skyfynd.io instead of localhost

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 // Base URL for the main Skyfynd site
-const SKYFYND_BASE_URL = "http://localhost:5176";
+const SKYFYND_BASE_URL = "https://skyfynd.io";
 
 // Custom hook to detect window width
 function useWindowWidth() {
